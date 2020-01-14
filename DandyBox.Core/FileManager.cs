da68a5ef109439bb81b483b8bea8b6515fd1d1c3
raawaa <wenjie_yu@yeah.net>
2020-01-14 15:20:25 +0800

    递归获取文件夹下所有媒体文件。

---
+++ DandyBox.Core/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace DandyBox.Core
+{
+    public static class FileManager
+    {
+        /// <summary>
+        /// 递归获取文件夹下的所有媒体文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetMediaFiles(string path)
+        {
+
+            string[] mediaExtensions = { ".avi" };
+
+
+            // 考虑文件夹访问权限。如果用户选择了一个没有访问权限的文件夹……
+            var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
+            var mediaFiles = from file in files
+                             where mediaExtensions.Contains(Path.GetExtension(file))
+                             select file;
+
+
+            return mediaFiles;
+        }
+    }
+}
---
+++ DandyBox.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using DandyBox.Core;
+
+
+namespace DandyBox.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DandyBox\\");
+            //Console.WriteLine(path);
+
+            var files = FileManager.GetMediaFiles(@"E:\俞文杰手机微信文件备份\");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.WriteLine("END OF INFO");
+            Console.ReadLine();
+        }
+    }
+}
