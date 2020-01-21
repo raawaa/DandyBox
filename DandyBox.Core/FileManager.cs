de40339bf0f3ddb8e026282cfabff5b6d1e354c8
raawaa <wenjie_yu@yeah.net>
2020-01-21 16:08:08 +0800

    Clean codebase

--- DandyBox.Core/FileManager.cs
+++ DandyBox.Core/FileManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using DandyBox.Core.DataModels;
 
 namespace DandyBox.Core
 {
@@ -20,20 +19,10 @@ namespace DandyBox.Core
             // TODO: 考虑文件夹访问权限。如果用户选择了一个没有访问权限的文件夹……
             IEnumerable<string> files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
             IEnumerable<FileInfo> mediaFiles = from file in files
-                                                where mediaExtensions.Contains(Path.GetExtension(file))
-                                                select new System.IO.FileInfo(file);
-                                             //select new MediaFile { FilePath=file};
+                                               where mediaExtensions.Contains(Path.GetExtension(file))
+                                               select new System.IO.FileInfo(file);
+            //select new MediaFile { FilePath=file};
             return mediaFiles.ToList();
         }
-
-        
-
-        /// <summary>
-        /// Return the first match of the regString.
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="regString"></param>
-        /// <returns></returns>
-        
     }
 }
